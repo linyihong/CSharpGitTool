ee7c66f2fb3b3f56fed006e10a46cf107f00cc29
larrylin <larry.lin@frog-jump.com>
2020-07-13 15:32:55 +0800

    Add TestWeb Project

---
+++ TestWeb/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using GitTools;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var gitHelper = new GitHelper();
+            var projectNames = gitHelper.ProjectNames;
+            ViewData["ProjectNames"] = projectNames;
+
+            return View();
+        }
+    }
+}
